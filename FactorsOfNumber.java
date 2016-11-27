5e9454f8b2d9fee12521519a08daf70aab98dade
Novjean J Kannathara <novjeanj@gmail.com>
2016-11-26 21:43:49 -0800

    Create FactorsOfNumber.java

---
+++ FactorsOfNumber.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorsofnumber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Novjean
+ */
+public class FactorsOfNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num = 1;
+        
+        System.out.println("Enter the number: ");
+        Scanner in  = new Scanner(System.in);
+        num = in.nextInt();
+        
+        System.out.print("Factors of " +num+ " are: ");
+        for(int i=1 ; i<= num; i++)
+        {
+            if(num%i == 0)
+                System.out.print(" "+i);
+        }
+        
+    }
+    
+}
