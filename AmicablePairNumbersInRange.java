a9fd5f92737ff35cc15b5947f067a44959dc78c1
Novjean J Kannathara <novjeanj@gmail.com>
2016-11-30 16:51:58 -0800

    Create AmicablePairNumbersInRange.java

---
+++ AmicablePairNumbersInRange.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printamicableperfectnumbersinrange;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Novjean
+ */
+public class PrintAmicablePerfectNumbersInRange {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter the first number: ");
+        int num1 = in.nextInt();
+        System.out.println("Enter the second number: ");
+        int num2 = in.nextInt();
+        
+        for(int i = num1; i<=num2; i++ )
+        {
+            for( int j =i+1; j<=num2; j++)
+            {
+                amicablePair(i,j);
+            }
+        }
+       
+    }
+    
+    public static void amicablePair(int num1, int num2)
+    {
+        //Find the factors and sum them. 
+        int sumFactorsNum1 = sumFactors(num1);
+        //System.out.println("sumFactorsNum1: " + sumFactorsNum1);
+        int sumFactorsNum2 = sumFactors(num2);
+        //System.out.println("sumFactorsNum2: " + sumFactorsNum2);
+        
+        if((sumFactorsNum1 == num2) &&(sumFactorsNum2 == num1))
+            System.out.println(num1 + " & " + num2 + " are AMICABLE.");
+        //else
+           // System.out.println(num1 + " & " + num2 + " are not amicable.");
+    }
+    
+    public static int sumFactors(int num)
+    {
+        int sum = 0;
+        int divisor = num-1;
+        
+        while(divisor>0)
+        {
+            //System.out.println(num%divisor);
+            if((float)(num%divisor) == 0.0)
+            {
+                //System.out.println(divisor + " is a factor of " + num);
+                sum += divisor;
+            }
+            divisor--;
+        }
+        return sum;
+    }
+    
+}
