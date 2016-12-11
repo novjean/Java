4aa5f9d213cb0747370b20de5528ddda38cbd50e
Novjean J Kannathara <novjeanj@gmail.com>
2016-12-11 00:17:48 -0800

    Update BubbleSort.java
    created a better bubblesort with flag

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,13 +1,4 @@
-
-/*
-elemets sorted in asc of desc orger by comparing adjacent elemnts
-and place them on asc or desc order
-if we have n elements, we require n-1 passes
-*/
-
 package bubblesort;
-
-import java.util.Scanner;
 
 /**
  *
@@ -15,43 +6,37 @@ import java.util.Scanner;
  */
 public class BubbleSort {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        int[] arr = {9,8,5,7,6};
-        System.out.println("Before sorting: ");
-        
-        for(int i: arr)
-        {
+    public static void DisplayArray(int[] arr){
+        for(int i: arr)    
             System.out.println(i);
-        }
-        
-        bubbleSort(arr);
-        
-        System.out.println("After sorting: ");
-        
-        for(int i:arr)
-        {
-            System.out.println(i);
+    }
+    
+    public static void BubbleSort(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            int flag = 0;
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = 1;
+                }
+            }
+            if(flag == 0){
+                break;
+            }
         }
     }
     
-    public static void bubbleSort(int[] arr)
-    {
-        int temp=0;
-        for(int i=0; i<arr.length; i++)
-        {
-            for(int j=1;j<(arr.length) - i; j++)
-            {
-                if(arr[j-1]>arr[j])
-                {
-                    temp = arr[j-1];
-                    arr[j-1]=arr[j];
-                    arr[j] = temp;
-                }
-            }
-        }
+    public static void main(String[] args) {
+        int[] arr = {5,1,6,2,4,3,52,32,1,2,42,6,7,5,32,9,60,7,54};
+      
+        System.out.println("Before sorting: ");
+        DisplayArray(arr);
+        
+        BubbleSort(arr);
+        
+        System.out.println("After sorting: ");
+        DisplayArray(arr);
     }
 }
